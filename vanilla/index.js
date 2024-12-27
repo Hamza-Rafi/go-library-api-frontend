65550680086ac65da671557da8716314fa47f475
Hamza Rafi <rafi.hamza2020@gmail.com>
2024-12-27 20:42:43 +0000

    refactored table population and post handler

--- vanilla/index.js
+++ vanilla/index.js
@@ -2,7 +2,7 @@ var apiBase = "https://library.hamzarafi.com"
 
 var booksTable = document.getElementById("booksTable")
 
-async function getData() {
+async function getBooks() {
     var url = apiBase + '/books'
 
     var response = await fetch(url)
@@ -11,17 +11,36 @@ async function getData() {
     return json
 }
 
+async function postBook(title, author) {
+    var url = apiBase + '/books'
+
+    body = {
+        title: title,
+        author: author
+    }
+
+    const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(body)
+    })
+
+    const newBook = await response.json()
+    return newBook
+}
+
+function addRow(book, rowNumber) {
+    var row = booksTable.insertRow(rowNumber)
+
+    row.insertCell(0).innerText = book.id
+    row.insertCell(1).innerText = book.title
+    row.insertCell(2).innerText = book.author
+}
 async function populateTable() {
-    var books = await getData()
+    var books = await getBooks()
 
     books.forEach((book, count) => {
-        var row = booksTable.insertRow(count)
-
-        row.insertCell(0).innerText = book.id
-        row.insertCell(1).innerText = book.title
-        row.insertCell(2).innerText = book.author
-
         //row.insertCell(3).innerHTML = '<button>edit</button>'
+        addRow(book, count)
     })
 
     // populate table headers
@@ -35,33 +54,17 @@ async function populateTable() {
 }
 
 async function addBook() {
-    var url = apiBase + '/books'
-
     var titleInput = document.getElementById('input-title')
     var authorInput = document.getElementById('input-author')
 
-    body = {
-        title: titleInput.value,
-        author: authorInput.value
-    }
+    newBook = await postBook(titleInput.value, authorInput.value)
 
-    const response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(body)
-    })
-
-    const newBook = await response.json()
-    ''
     // add new book to table
-    var newRow = booksTable.insertRow(booksTable.rows.length)
-    newRow.insertCell(0).innerText = newBook.id
-    newRow.insertCell(1).innerText = newBook.title
-    newRow.insertCell(2).innerText = newBook.author
+    addRow(newBook, booksTable.rows.length)
 
     // clear inputs
     titleInput.value = ''
     authorInput.value = ''
-
 }
 
 populateTable()

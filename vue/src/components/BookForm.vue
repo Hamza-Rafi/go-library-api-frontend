fa3ecdb5c597bd8d592d7bcd774b6e7355e1e06e
Hamza Rafi <rafi.hamza2020@gmail.com>
2024-12-28 05:21:32 +0000

    completed vue frontend

---
+++ vue/src/components/BookForm.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useBooksStore } from '@/stores/books';
+const bookStore = useBooksStore()
+
+let title = ref('')
+let author = ref('')
+
+</script>
+
+<template>
+  <input type="text" v-model="title" placeholder="Book Title" required>
+  <input type="text" v-model="author" placeholder="Book Author" required>
+  <button @click="bookStore.addBook(title, author)">Add Book</button>
+</template>
---
+++ vue/src/components/BooksTable.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useBooksStore } from '@/stores/books';
+import Book from './Book.vue';
+
+const booksStore = useBooksStore()
+booksStore.getBooks()
+
+</script>
+
+<template>
+  <table>
+    <tr>
+      <th>id</th>
+      <th>title</th>
+      <th>author</th>
+    </tr>
+
+    <Book v-for="book in booksStore.books" :id="book.id" :title="book.title" :author="book.author" />
+  </table>
+</template>
+
+<style>
+table {
+  width: 50%;
+  border-collapse: collapse;
+  margin: 1rem;
+}
+
+td {
+  border-left: 1px solid black;
+  margin: 0;
+  padding-left: 1rem;
+}
+
+td:first-child {
+  border-left: none;
+}
+
+th {
+  border-bottom: 1px solid black;
+}
+</style>

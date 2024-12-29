5475cc6127df692a10c31c36636a8895549d2c19
Hamza Rafi <rafi.hamza2020@gmail.com>
2024-12-29 06:56:37 +0000

    added form to create book

--- react/src/BookTable.tsx
+++ react/src/BookTable.tsx
@@ -1,35 +1,38 @@
 import { useState } from "react"
-import Book from "./Book"
 import { url } from "./bookUtils"
+import Book from "./Book"
+import AddBookForm from "./AddBookForm"
 
 function BookTable() {
-    const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([])
 
-    async function getBooks() {
-        let response = await fetch(url)
-        let bookData = await response.json()
+  async function getBooks() {
+    let response = await fetch(url)
+    let bookData = await response.json()
 
-        setBooks(bookData)
-    }
+    setBooks(bookData)
+  }
 
-    if (books.length == 0) {
-        getBooks()
-    }
+  if (books.length == 0) {
+    getBooks()
+  }
 
-    return (
-        <>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>id</th>
-                        <th>title</th>
-                        <th>author</th>
-                    </tr>
-                    {books.map(book => <Book key={book.id} id={book.id} title={book.title} author={book.author} />)}
-                </tbody>
-            </table>
-        </>
-    )
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>author</th>
+          </tr>
+
+          {books.map(book => <Book key={book.id} id={book.id} title={book.title} author={book.author} />)}
+        </tbody>
+      </table>
+      <AddBookForm setBooks={setBooks} books={books} />
+    </>
+  )
 }
-
 export default BookTable
+
--- react/src/bookUtils.ts
+++ react/src/bookUtils.ts
@@ -1,9 +1,2 @@
 export let apiBase = "https://library.hamzarafi.com"
 export let url = apiBase + '/books'
-
-//export async function getBooks() {
-//  let response = await fetch(url)
-//  let books = await response.json()
-//
-//  return books
-//}
